6e5613aa89e196806f9bf6777a67c7b3e6516d30
Abdulazeez Sodiq <70654281+Abdulzeez403@users.noreply.github.com>
2022-09-05 17:38:42 -0700

    initializing responsive next

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -9,40 +9,41 @@ const HomePage = () => {
   return ( 
 
    <div  className="HomeImages">
-<div className="subHomestyle">
-<div className="flex justify-around">
+  <br/>  <br/>  <br/>
 
-<div className = "gap-3 w-[50%]">
-<h4 className=" text-[3.2rem] font-bold ">Discover, collect, and sell  extraordinary NFTs</h4>
+<div className="subHomestyle">
+<div className="block  md:block lg:flex lg:justify-around">
+<div className = "gap-y-10 lg:gap-3 lg:w-[50%]">
+<h4 className="text-center text-[1.6rem] lg:text-[3.2rem] font-bold ">Discover, collect, and sell  extraordinary NFTs</h4>
 <br/>
-  <p className='text-[2rem] w-[60%] '>OpenSea is the world's first and largest NFT marketplace</p>
+  <p className='text-center text-[1rem]  lg:text-[2rem] lg:w-[60%] '>OpenSea is the world's first and largest NFT marketplace</p>
   <br/>
 
-  <div className="gap-6 flex ">
+  <div className="gap-6 flex justify-center mb-5 ">
     <button className="w-[8rem] h-[4rem] rounded-lg bg-blue-200"> Explore</button>
     <button className="w-[8rem] h-[4rem] rounded-lg shadow-lg"> Create</button>
   </div>
 
 </div>
 
-<div className="  bg-white rounded-0 rounded-tl-lg rounded-tr-lg" >
+    <div className="m-3 bg-red-100 lg:m-0 rounded-lg "  >
   <Image src={HomeImage}
   alt="Images"
   width={500} 
   height={400}/>
 
-  <div className=" flex justify-between  shadow-lg  rounded-bl-lg  rounded-br-lg  ">
-    <div className="mx-4">
+  <div className=" flex justify-between shadow-lg mx-6">
+    <div>
     <Image src={HomeImage}
   alt="Images"
   width={40} 
   height={40}
   className="rounded-full"/>
 <div>
-<h4>SpaceFace</h4>
-<h4>Adamtoksoz</h4>
+<h4  className="text-lg font-bold">SpaceFace</h4>
+<h4 className="text-blue-400">Adamtoksoz</h4>
 </div>
-    </div>
+</div>
 <CgDanger color='grey' size={20}/>
   </div>
 
